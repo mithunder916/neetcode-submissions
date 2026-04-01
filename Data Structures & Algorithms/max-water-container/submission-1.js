1b050fc8f6fb46af4eb18927e31e867bc7cd15fd
Mithun Selvaratnam <mselvaratnam@gmail.com>
2026-04-01 11:23:27 -0400

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        // max water is determined by height of shorter bar * (right idx - left idx)
+        // move min height of the two
+        let left = 0;
+        let right = heights.length - 1;
+        let maxWater = 0;
+
+        while (left < right) {
+            const water = Math.min(heights[left], heights[right]) * (right - left);
+            maxWater = Math.max(water, maxWater);
+
+            if (heights[left] <= heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}

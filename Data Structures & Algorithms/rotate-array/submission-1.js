dc062136d173afdd77dc8a8362bc190f99c1a594
Mithun Selvaratnam <mselvaratnam@gmail.com>
2026-04-02 10:58:06 -0400

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    rotate(nums, k) {
+        // new index: i + k; when i + k > nums.length - 1, do % nums.length
+        // how to move values cleanly without overwriting?
+        const n = nums.length;
+        k %= n;
+        let count = 0;
+
+        // repeat n times; move n elements (everything in array)
+        for (let start = 0; count < n; start++) {
+            // move first element first
+            let prev = nums[start];
+            // index to move to
+            let current = (start + k) % n;
+
+            
+            while (current !== start) {
+                // store element about to be displaced
+                const temp = nums[current];
+                // replace previous element
+                nums[current] = prev;
+                prev = temp;
+                // update next position to move
+                current = (current + k) % n;
+                // increment count
+                count++;
+            }
+            // place the last displaced element back at start
+            nums[start] = prev;
+            count++;
+        }
+    }
+}

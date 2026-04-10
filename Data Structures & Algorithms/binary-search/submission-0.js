0e53251991fc90f27807108e9702a91952989a6e
Mithun Selvaratnam <mselvaratnam@gmail.com>
2026-04-10 10:20:32 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+        let mid;
+
+        while (left <= right) {
+            mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return -1;
+    }
+}

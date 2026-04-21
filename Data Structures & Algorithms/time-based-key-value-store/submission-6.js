e1fd7556da0ae196dd113a3ac0a7a4973f5259f9
Mithun Selvaratnam <mselvaratnam@gmail.com>
2026-04-21 13:40:23 -0400

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.js
@@ -0,0 +1,44 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        const values = this.keyStore.get(key);
+        values ? values.push([timestamp, value]) : this.keyStore.set(key, [[timestamp, value]]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        // binary search 
+        if (!this.keyStore.get(key)) return "";
+        const values = this.keyStore.get(key);
+
+        let left = 0;
+        let right = values.length - 1;
+        let result = "";
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (values[mid][0] === timestamp) return values[mid][1];
+            else if (values[mid][0] < timestamp) {
+                result = values[mid][1];
+                left = mid + 1
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}

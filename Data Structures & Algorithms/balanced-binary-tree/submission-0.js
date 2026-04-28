e8caa506a6719508b83dc88d3eae85dd6613c7cc
Mithun Selvaratnam <mselvaratnam@gmail.com>
2026-04-28 15:21:16 -0400

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        return height(root) !== -1;
+
+        function height(node) {
+            if (!node) return 0;
+
+            const leftHeight = height(node.left);
+            if (leftHeight === -1) return -1;
+
+            const rightHeight = height(node.right);
+            if (rightHeight === -1) return -1;
+
+            if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+
+    }
+}
